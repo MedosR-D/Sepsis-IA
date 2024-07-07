c7a9674f65fd9404468ea88f8bf311f3fd373d38
abernard <abernard83@myges.fr>
2024-07-07 23:08:27 +0200

    CORS files

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 from typing import Union
 
 from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, PositiveInt
 from datetime import datetime, timedelta
 
@@ -15,7 +16,8 @@ total_patient_positive = total_patient_negative = 0
 time_to_predict = timedelta()
 
 app = FastAPI(openapi_url=None)
-
+origins = ["*"]
+app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
 class Patient(BaseModel):
     prg: int
